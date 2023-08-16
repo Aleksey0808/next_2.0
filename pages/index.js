2f40dc51098d8bb586d9fff192c283817254c887
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-08-16 22:35:57 +0300

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Heading from "@/components/Heading";
+import styles from "../styles/Home.module.scss";
 
 const Home = () => (
-  <Heading text= "Hello Next.js"/>
+  <div className={styles.wrapper}>
+      <Heading text= "Hello Next.js"/>
+  </div>
 );
 
 export default Home;
